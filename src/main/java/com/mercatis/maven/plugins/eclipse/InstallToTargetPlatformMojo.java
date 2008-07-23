966438b538cd299bd13045dcc4ec6a5a4a4f93c5
volker.fritzsch <volker.fritzsch@49add585-ac52-0410-9482-e9145766a8a9>
2008-07-23 22:24:10 +0000

    added NOTICE and changed copyright

--- src/main/java/com/mercatis/maven/plugins/eclipse/InstallToTargetPlatformMojo.java
+++ src/main/java/com/mercatis/maven/plugins/eclipse/InstallToTargetPlatformMojo.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2008 Volker Fritzsch
+ * Copyright 2008 mercatis
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
